9bd005230b35c94bacb4b8f67d924854fba5bbe2
selvackum <selvackumar@outlook.com>
2025-02-13 15:00:47 +0100

    sa

--- main.py
+++ main.py
@@ -1,22 +1,10 @@
 import datetime
-import os
-import logging
+
 from frcm.frcapi import METFireRiskAPI
 from frcm.datamodel.model import Location
-from dotenv import load_dotenv
-
-# Load environment variables from .env file
-load_dotenv()
-
-# Configure logging
-logging.basicConfig(level=logging.DEBUG)
 
 # sample code illustrating how to use the Fire Risk Computation API (FRCAPI)
 if __name__ == "__main__":
-    # Check if MET_CLIENT_ID is loaded
-    met_client_id = os.getenv('MET_CLIENT_ID')
-    if not met_client_id:
-        raise ValueError("MET_CLIENT_ID is not set. Please check your .env file.")
 
     frc = METFireRiskAPI()
 
@@ -26,18 +14,9 @@ if __name__ == "__main__":
     # days into the past to retrieve observed weather data
     obs_delta = datetime.timedelta(days=2)
 
-    try:
-        wd = frc.get_weatherdata_now(location, obs_delta)
-        print(wd)
-    except KeyError as e:
-        logging.error(f"KeyError: {e}")
-        logging.error("API response did not contain expected 'data' key.")
-        raise
+    wd = frc.get_weatherdata_now(location, obs_delta)
+    print (wd)
 
-    try:
-        predictions = frc.compute_now(location, obs_delta)
-        print(predictions)
-    except KeyError as e:
-        logging.error(f"KeyError: {e}")
-        logging.error("API response did not contain expected 'data' key.")
-        raise
+    predictions = frc.compute_now(location, obs_delta)
+
+    print(predictions)
